d132b5b8043d208d1a2951d7a0b93615be2ddbb7
Elmira Skorobogatova <elmirasco@gmail.com>
2021-06-23 09:50:51 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const Widget = getElementById('first-widget')
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App domElement={Widget}/>
   </React.StrictMode>,
-  document.getElementById('root')
+  Widget
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
