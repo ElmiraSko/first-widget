19909466b00c95588c6dafabebf299664b1cb763
Elmira Skorobogatova <elmirasco@gmail.com>
2021-06-23 13:00:33 +0200

    add ;

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import React from 'react';
 import './App.css';
 
 function App({ domElement }) {
-  const attr = domElement.getAttribute("id")
+
+  const attr = domElement.getAttribute("id");
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        
+    <div className="App">         
         <h1 style={{textAlign: 'center'}}>
           My First widget width id = {attr}
-        </h1>
-      
-      </header>
+        </h1>     
     </div>
   );
 }
